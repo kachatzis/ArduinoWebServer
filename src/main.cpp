13e73446119080077e47549f856e11301ec02fce
Konstantinos Chatzis <kostaschatzis01@gmail.com>
2019-03-01 23:52:43 +0200

    Update Main

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 
 
 
-/*
 byte PHY_ADDRESS[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress IP_ADDRESS(10, 3, 141, 103);
 IPAddress DNS_ADDRESS(10, 3, 141, 10);
@@ -16,16 +15,13 @@ IPAddress SUBNET(255, 255, 255, 0);
 EthernetServer server(80); 
 WebServer webServer;
 
-String readString;
 
-int pos = 0; 
-*/
 void setup() {
   
   Serial.begin(9600);
    while (!Serial) {;}
 
-  /*
+  
   Serial.println("Starting Ethernet");
 
   Ethernet.begin(PHY_ADDRESS,IP_ADDRESS, DNS_ADDRESS, GATEWAY_ADDRESS, SUBNET);
@@ -35,31 +31,32 @@ void setup() {
   Serial.println(Ethernet.localIP());
   
   webServer = WebServer(server);
-  */
+  
 }
 
 
-String func_e1( String a ){
-  return( "{\"a\":\"" + a + "\"}" );
+String func_e1(WebRequest webRequest){
+  return "E1";
 }
 
+String func_e2(WebRequest webRequest){
+  return "E2";
+}
 
 
 void loop() {
-  
-  /*ApiEndpoint (*apiEndpoints)[1];
-  
-  (*apiEndpoints)[0].setUrl("/e1")
-    .setRequiredParameters( new String[1]{"a"} , 1 ) 
-    .setWebPage( WebPage().setTitle("/e1").setBody("/e1") )
-    .setFunction( &func_e1 );
 
-  Serial.println("  >LOADING-API-SERVER");
-  ApiServer apiServer = ApiServer( *apiEndpoints , 1 , server );
+  ApiEndpoint *apiEndpoints = new ApiEndpoint[2];
+  apiEndpoints[0].setUrl("/e1")
+    .setRequiredParameters( new String[1]{"a"} , 1 ) 
+    .setFunction( &func_e1 );
+  apiEndpoints[1].setUrl("/e2")
+    .setRequiredParameters( new String[1]{"a"} , 1 ) 
+    .setFunction( &func_e2 );
+
+  ApiServer apiServer = ApiServer( apiEndpoints , 2 , server );
   apiServer.runFullThread();
-  */
-  Serial.println("  >555555555555");
-  //Serial.println( WEBPAGE_H );
-  delay(500);
+  
+  delay(500); // Wait 500ms before re-initializing the loop
 }
 
