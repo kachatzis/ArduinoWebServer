4ca1ef9f3febfd1a10ca8605d2f8dfe3cc2a8812
Konstantinos Chatzis <kostaschatzis01@gmail.com>
2019-03-01 23:45:17 +0200

    Revert "Add getWebRequest method"
    This reverts commit cd50b32409555590fe6f63327df1549f6c60a0a3.

--- lib/EthernetWeb/src/WebServer.cpp
+++ lib/EthernetWeb/src/WebServer.cpp
@@ -13,54 +13,22 @@ WebServer::WebServer( EthernetServer ethernetServer ){
 }
 
 WebRequest WebServer::getWebRequest(){
-
-  EthernetClient client;
-  client = this->ethernetServer.available();
-
-  if (client) {
-
-    String readString;
-    int maxLength = 50;
-
-    while (client.connected() && maxLength>0) { 
-
-      if (client.available()) {
-
-        char c = client.read();
-        readString += c;
-
-        if (readString.indexOf("\n")>0){
-
-          if (readString.indexOf("GET")>=0){
-            readString.trim();
-
-            WebRequest webRequest( readString , client );
-            return webRequest;
-
-          }else{
-
-            return WebRequest();
-
-          }
+  // EthernetClient client;
+  // client = this->ethernetServer.available();
+  // if (client) {
+  //   String readString;
+  //   int maxLength = 50;
+  //   while (client.connected() && maxLength>0) {   
+  //     if (client.available()) {
+  //       char c = client.read();
+  //       readString += c;
+  //       if (readString.indexOf("\n")>0){
+  //         Serial.print("  >REQUESTED:");
+  //         Serial.println(readString);
+  //         client.stop();
+  //         WebRequest webRequest( readString , client );
+  //         return webRequest;
           
-        }
-
-      }
-
-      maxLength--;
-
-    }
-
-    client.stop();
-
-    return WebRequest();
-
-  }
-
-  client.stop();
-  return WebRequest();
-
-}
   //       }
   //     }
   //     maxLength--;
